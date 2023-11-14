2d6b358aa7087a46d46f547b23bbb1ed8b8ff761
ParivalavanIT <112223851+ParivalavanIT@users.noreply.github.com>
2023-11-14 15:57:59 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
-function App() {
-
+import React from 'react'
+import Nav from './components/Nav'
+import Jumbotron from './components/Jumbotron'
+import SoundSection from './components/SoundSection'
+const App = () => {
   return (
+
     <div className="App">
-      Hello
+      <Nav />
+      <Jumbotron /> 
+      <SoundSection />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/components/Nav.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Logo from '../assets/images/logo.svg'
+import Search from '../assets/images/search.svg'
+import Store from '../assets/images/store.svg'
+const Nav = () => {
+  return (
+    <nav className='nav-wrapper'>
+        <div className="nav-content">
+            <ul className='list-styled'>
+                <img src={Logo} alt="logo" />
+                <li>
+                    <a className='link-styled'href="#">Store</a>
+                </li>
+                  <li>
+                      <a className='link-styled'href="#">Mac</a>
+                  </li>
+                  <li>
+                      <a className='link-styled'href="#">iPad</a>
+                  </li>
+                  <li>
+                      <a className='link-styled'href="#">iPhone</a>
+                  </li>
+                  <li>
+                      <a className='link-styled'href="#">Watch</a>
+                  </li>
+                  <li>
+                      <a className='link-styled'href="#">AirPods </a>
+                  </li>
+                  <li>
+                      <a className='link-styled'href="#">Tv & Home</a>
+                  </li>
+                  <li>
+                      <a className='link-styled'href="#">Entertainment</a>
+                  </li>
+                  <li>
+                      <a className='link-styled'href="#">Accesories</a>
+                  </li>
+                  <li>
+                      <a className='link-styled'href="#">Support</a>
+                  </li>
+                  <li>
+                      <img src={Search} alt="Search" />
+                  </li>
+                  <li>
+                      <img src={Store} alt="Store" />
+                  </li>
+
+            </ul>
+        </div>
+      
+    </nav>
+  )
+}
+
+export default Nav
